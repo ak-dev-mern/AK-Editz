a917b4faf978706e43423e26777eb0e83957c1a3
Ajith Kumar <ak.dev.mern@gmail.com>
2025-11-05 22:46:03 +0800

    add new api end point

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://ak-editz.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://ak-editz.onrender.com/api",
   timeout: 30000,
   withCredentials: true, // Important for cookies
 });
