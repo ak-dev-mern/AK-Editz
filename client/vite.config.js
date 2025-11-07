196ff512dd09fb8386884cc3b3722a5245140a09
Ajith Kumar <akeditzdj@gmail.com>
2025-11-07 10:26:20 +0800

    update backend url

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "https://akeditz.com",
+        target: "https://ak-editz.onrender.com",
         changeOrigin: true,
         secure: false,
       },
