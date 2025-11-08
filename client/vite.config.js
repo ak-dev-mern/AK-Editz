519fa226caf404f15d8d4c844b820925c7666ea3
Ajith Kumar <ak.dev.mern@gmail.com>
2025-11-08 22:54:52 +0800

    update vite config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
-    // proxy: {
-    //   "/api": {
-    //     target: "http://localhost:5000", // LOCAL ONLY
-    //     changeOrigin: true,
-    //   },
-    // },
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // only for local dev
+        changeOrigin: true,
+      },
+    },
   },
+
   build: {
     outDir: "dist",
   },
